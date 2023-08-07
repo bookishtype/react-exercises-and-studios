9e697138c35295a6a5d424439928a06ee9e6b2aa
Faith <fmschroeder98@gmail.com>
2023-08-07 18:10:08 -0500

    Studio 2

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,9 +1,15 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
-  return stars;
+function RateARecipe(props) {
+  function GiveRating() {
+    return <h3>{stars[props.rating - 1]}</h3>;
+ }
+ //Create a ternary that checks the value of props.
+ //If the number is between 1 and 5, then render GiveRating else render null
+ return props.rating >= 1 && props.rating <= 5 ? <GiveRating /> : null;
 }
 
 export default RateARecipe;
+
 
 //conditional to render stars based on a number provided in App.js 
